2a6ddcede6fa525dc55cceff22d76901dab8fe17
liangquan <liangquan0000@outlook.com>
2023-09-18 08:12:55 +0800

    09/18-01

---
+++ StudyCollection/src/main/java/com/quan/jihe/A01_CollectionDemo1.java
@@ -0,0 +1,12 @@
+package com.quan.jihe;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class A01_CollectionDemo1 {
+
+    Collection<String> collection = new ArrayList<>();
+
+
+
+}
