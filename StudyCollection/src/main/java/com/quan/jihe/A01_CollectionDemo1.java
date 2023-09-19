640d6a71534358dde4179f5465192c8aea02ed9f
liangquan <liangquan0000@outlook.com>
2023-09-19 09:55:01 +0800

    09/19-01

--- StudyCollection/src/main/java/com/quan/jihe/A01_CollectionDemo1.java
+++ StudyCollection/src/main/java/com/quan/jihe/A01_CollectionDemo1.java
@@ -2,11 +2,27 @@ package com.quan.jihe;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class A01_CollectionDemo1 {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("1");
+        collection.add("2");
+        collection.add("3");
+        System.out.println("collection.isEmpty() = " + collection.isEmpty());
 
-    Collection<String> collection = new ArrayList<>();
+        // 迭代器
+        Iterator<String> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println("next = " + next);
+        }
 
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println("next = " + next);
+        }
 
-
+    }
 }
